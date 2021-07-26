b04ced6d9bd0f15a5e74244c35f6d0f97f50f4ad
Yat <63180414+yat2k@users.noreply.github.com>
2021-07-26 14:24:37 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Player1 player = new Player1("Yathin","sword",100);
+//      System.out.println(player.getName());
+//      System.out.println(player.getHealth());
+//      System.out.println(player.getWeapon());
+
+
+	}
+
+}
